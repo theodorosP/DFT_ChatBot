45b6b9f7d59f137003a3b233e875fe97c087fee0
thodorisP <teosfp@hotmail.com>
2024-01-25 15:00:14 -0500

    commit

--- simple_DFT_bot.py
+++ simple_DFT_bot.py
@@ -1,30 +1,53 @@
-import nltk
-from nltk.chat.util import Chat, reflections
+class DFTChatBot:
+	# Define constructor
+	def __init__(self, patterns):
+		self.patterns = patterns
 
-patterns = [
-    (r'Hi|hello|hey', ["Hi there! Are you ready to learn about Density Functional Theory?",
-                      "Hello! Are you ready to learn about Density Functional Theory?",
-                      "Hey! Are you ready to learn about Density Functional Theory?"]),
-]
+	def print_text(self):
+		print("Hello! I am a Density Functional Theory (DFT) chatbot.")
+		print("If you want to quit, please type 'quit' or 'exit.'")
 
-chatbot = Chat(patterns, reflections)
+	def create_bot(self):
+		import nltk
+		from nltk.chat.util import Chat, reflections
+		chatbot = Chat(self.patterns, reflections)
+		return chatbot
 
-print("Hello! I am a Density Functional Theory (DFT) chatbot.")
-print("If you want to quit, please type 'quit' or 'exit.' ")
+	def get_user_input(self):
+		return input("You: ")
 
-response_list = []  # Variable to store responses
+	def exit_bot(self, user_input):
+		if user_input.lower() == "quit" or user_input.lower() == "exit":
+			print("Chatbot: Goodbye!")
+			return True  
+		return False
 
-while True:
-    user_input = input("You: ")
-    if user_input.lower() == "quit" or user_input.lower() == "exit":
-        print("Chatbot: Goodbye!")
-        break
+	def chat_with_bot(self, chatbot):
+		response_variable = ""
+		while True:
+			user_input = self.get_user_input()
+			if self.exit_bot( user_input ):
+				break
 
-    
-    response = chatbot.respond(user_input)
-    response_list.append(response)
-    if response:
-        print("Chatbot:", response)
-    if user_input == "no" and "Are you ready to learn about Density Functional Theory?" in response_list[ len(response_list) - 2]:
-        print("I am sorry but I am only trained to talk about DFT. Goodbye!")
-        break
+			response = chatbot.respond(user_input)
+			if response:
+				print("Chatbot:", response)
+			if user_input == "no" and "Are you ready to learn about Density Functional Theory?" in response_variable:
+				print("I am sorry but I am only trained to talk about DFT. Goodbye!")
+				break
+
+			response_variable = response
+
+
+if __name__ == "__main__":
+	patterns = [
+		(r'Hi|hello|hey', ["Hi there! Are you ready to learn about Density Functional Theory?",
+							"Hello! Are you ready to learn about Density Functional Theory?",
+							"Hey! Are you ready to learn about Density Functional Theory?"])
+	]
+
+	dft_bot = DFTChatBot(patterns)
+	dft_bot.print_text()
+
+	chatbot_instance = dft_bot.create_bot()
+	dft_bot.chat_with_bot(chatbot_instance)
